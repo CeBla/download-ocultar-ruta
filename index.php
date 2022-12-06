ef62445db5479f826d9db8625c128fd581448870
CeBla <86948017+CeBla@users.noreply.github.com>
2022-12-06 21:44:07 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+$filename = 'pdf/readme.pdf';
+
+if (file_exists($filename)) {
+header('Content-Description: File Transfer');
+header('Content-Type: application/octet-stream');
+header('Content-Disposition: attachment; filename="' . basename($filename) . '"');
+header('Expires: 0');
+header('Cache-Control: must-revalidate');
+header('Pragma: public');
+header('Content-Length: ' . filesize($filename));
+readfile($filename);
+exit;
+}
+?>
